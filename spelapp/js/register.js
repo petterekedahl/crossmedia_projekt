45e996aaacce920eaa0cd54dd64d52f713048997
Petter Ekedahl <petter.ekedahl@gmail.com>
2021-04-14 14:09:14 +0200

    Temporary update

---
+++ spelapp/js/register.js
@@ -0,0 +1,36 @@
+const regForm = document.getElementById('register-form');
+
+regForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const usernameDiv = document.getElementById('register-username');
+  const emailDiv = document.getElementById('register-email');
+  const passwordDiv = document.getElementById('register-password');
+  const password2 = document.getElementById('register-password2');
+
+  const username = usernameDiv.value;
+  const email = emailDiv.value;
+  const password = passwordDiv.value;
+
+  if(passwordDiv.value != password2.value) {
+    console.log("Passwords do not match");
+    return;
+  }
+
+  const newRegister = {
+    username,
+    email,
+    password
+  };
+
+  const regReq = new Request('../database/admin/register.php', {
+    method: "POST",
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify(newRegister)
+  });
+  fetch(regReq)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+    })
+})
