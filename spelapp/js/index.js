4bf302f4e1fb70a8bc6e95f38d51feb80f1857bd
Petter Ekedahl <petter.ekedahl@gmail.com>
2021-04-16 16:13:17 +0200

    Update menu functionality
    Update menu funtionality and some js variables to make the switch work as expected.

--- spelapp/js/index.js
+++ spelapp/js/index.js
@@ -1,4 +1,5 @@
-let whatPageToLoad = 0;
+const mainDiv = document.querySelector('#main-div');
+
 //Tar bort "welcome" när sidan har laddats klart
 setTimeout(() => {
   const welcomeDiv = document.querySelector('.spel-welcome-div');
@@ -9,27 +10,7 @@ setTimeout(() => {
   setTimeout(() => {
     welcomeDiv.style.display = 'none';
     mainDiv.style.display = 'flex';
-    whatPageToLoad = loadNavClicks();
+    loadNavClicks();
   }, 1500);
 }, 500);
-
-switch (whatPageToLoad) {
-  case 0:
-    loadHomePage();
-    break;
-  case 1:
-    loadInformationPage();
-    break;
-  case 2:
-    loadProfilePage();
-    break;
-  case 3:
-    loadSuspectPage();
-    break;
-  case 4:
-    loadNotesPage();
-    break;
-  default:
-    break;
-}
 
--- spelapp/js/navigation.js
+++ spelapp/js/navigation.js
@@ -4,13 +4,34 @@ function loadNavClicks() {
   
   
   for (let i = 0; i < navLinks.length; i++) {
+    whatPageToLoad = 0;
     navLinks[i].addEventListener('click', () => {
       document.querySelector('.active').classList.remove('active');
       document.querySelector('.active-slidein-div').classList.remove('active-slidein-div');
       navLinks[i].querySelector('.nav-button').classList.add('active');
       navLinks[i].querySelector('.nav-slidein-div').classList.add('active-slidein-div');
 
-      return i;
+      whatPageToLoad = i;
+
+      switch (whatPageToLoad) {
+        case 0:
+          loadHomePage();
+          break;
+        case 1:
+          loadInformationPage();
+          break;
+        case 2:
+          loadProfilePage();
+          break;
+        case 3:
+          loadSuspectPage();
+          break;
+        case 4:
+          loadNotesPage();
+          break;
+        default:
+          break;
+      }
     })
   }
 }
