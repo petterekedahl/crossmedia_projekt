2acc1c748294687ae0355822293e32b11cc53704
Petter Ekedahl <petter.ekedahl@gmail.com>
2021-05-06 10:06:36 +0200

    Update tems and conditions
    Added terms an condi..

--- spelapp/pages/terms-and-conditions.php
+++ spelapp/pages/terms-and-conditions.php
@@ -4,13 +4,35 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/terms-and-conditions.css">
   <title>M.P.P. - T&Cs</title>
 </head>
 <body>
   <main>
     <h1>TERMS AND CONDITIONS</h1>
     <div>
-      <p>BERÄTTAR DU FÖR NÅGON ÄR DU NÄST PÅ TUR.</p>
+      <h2>Terms and conditions</h2>
+      <p>
+      These Terms and Conditions outline the rules and regulations for the use of the Ministry of Private Protection's website.
+      </p>
+      <p>
+      By accepting the Terms and Conditions stated on this page we assume you understand them and will follow them correctly, if not, action will be taken.
+      </p>
+      <h2>Terminology</h2>
+      <p>
+      The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: “You” and “Your” refers to you, the person who log on this website and is compliant to the Ministry of Private Protection's terms and conditions. “We”, “Our” and “Us”, refers to the Ministry of Private Protection.
+      </p>
+      <h2>Using the service</h2>
+      <p>
+      Unless otherwise stated, the Ministry of Private Protection owns the intellectual property rights for all material on the website. All intellectual property rights are reserved. You may access this from the Ministry of Private Protection's website for your own personal use subjected to restrictions set in these Terms and Conditions. Information on this site is confidential and it is strictly forbidden to any of the following:
+      </p>
+      <p>You must not:</p>
+      <ul>
+        <li>Republish material from the Ministry of Private Protection's website</li>
+        <li>Sell, rent or sub-license material from the Ministry of Private Protection's website</li>
+        <li>Reproduce, duplicate or copy material from the Ministry of Private Protection's website</li>
+        <li>Redistribute content from the Ministry of Private Protection's website</li>
+      </ul>
     </div>
     <button><a href="./register.php">Okay</a></button>
   </main>
